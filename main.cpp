d306911e32e49a134e40a320ad16239e896d888a
xJenny99x <xjenny99x@gmail.com>
2015-12-20 12:49:04 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#ifdef BRM_PLUGIN_META
+	class ai_suite
+	{
+		name = "AI Spawner / cleaner / master";
+		version = 1.00;
+		authors[] = {"nigel"};
+		description = "This is some crazy shit that'll spawn you shit and save FPS.";
+	};
+#endif
+
+#ifdef BRM_PLUGIN_FUNCTIONS
+	#include "Functions.cpp"
+#endif
